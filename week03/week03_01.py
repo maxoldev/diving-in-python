c5934ed99b1c439328f5a72b8bd8d534ae86d34e
Maxim Soloviev <maxim@omega-r.com>
2018-08-16 01:20:46 +0300

    week03_01: filereader; week03_02: inheritance

--- week03/week03_01.py
+++ week03/week03_01.py
@@ -1,3 +1,13 @@
 class FileReader:
-    pass
+    def __init__(self, path):
+        assert isinstance(path, str)
+        self.path = path
+
+    def read(self):
+        try:
+            with open(self.path, "r") as f:
+                content = f.read()
+                return content
+        except IOError:
+            return ""
 
---
+++ week03/week03_02.py
@@ -0,0 +1,49 @@
+import os
+import csv
+
+
+class CarBase:
+    def __init__(self, brand, photo_file_name, carrying):
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
+
+    def get_photo_file_ext(self):
+        return os.path.splitext(self.photo_file_name)
+
+
+class Car(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+        super().__init__(brand, photo_file_name, carrying)
+        self.passenger_seats_count = passenger_seats_count
+
+
+class Truck(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, body_whl):
+        super().__init__(brand, photo_file_name, carrying)
+        self.body_whl = body_whl
+        self.body_length, self.body_width, self.body_height = 0, 0, 0
+
+    def get_body_volume(self):
+        return self.body_length * self.body_width * self.body_height
+
+
+class SpecMachine(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, extra):
+        super().__init__(brand, photo_file_name, carrying)
+        self.extra = extra
+
+
+def get_car_list(csv_filename):
+    car_list = []
+
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            print(row)
+
+    return car_list
+
+
+get_car_list("cars.csv")
